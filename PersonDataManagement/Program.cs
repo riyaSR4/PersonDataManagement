5db4441fc084e1f01b6f0f133dda1acd2c681c8e
Riya Susan Reji <riyasusan.r@tvsnext.com>
2023-08-10 10:15:19 +0530

    [Riya] Add : Created List of Persons

--- PersonDataManagement/Program.cs
+++ PersonDataManagement/Program.cs
@@ -1,1 +1,47 @@
-﻿
+﻿using System;
+
+namespace PersonDataManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> persons = new List<Person>();
+            persons.Add(new Person()
+            {
+                SSN = 1,
+                Name = "a",
+                Address = "a",
+                Age = 10
+            });
+            persons.Add(new Person()
+            {
+                SSN = 2,
+                Name = "b",
+                Address = "b",
+                Age = 15
+            });
+            persons.Add(new Person()
+            {
+                SSN = 3,
+                Name = "c",
+                Address = "c",
+                Age = 20
+            });
+            persons.Add(new Person()
+            {
+                SSN = 4,
+                Name = "d",
+                Address = "d",
+                Age = 25
+            });
+            persons.Add(new Person()
+            {
+                SSN = 5,
+                Name = "e",
+                Address = "e",
+                Age = 30
+            });
+        }
+    }
+}
